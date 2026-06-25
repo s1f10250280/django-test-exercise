5794995be03723ef769a66ee971b4c146ec77ca1
1F10250280 <s1F102502801@iniad.org>
2026-06-25 11:44:27 +0900

    訂正4

--- todo/views.py
+++ todo/views.py
@@ -8,8 +8,10 @@ from todo.models import Task
 
 def index(request):
     if request.method == 'POST':
-        task = Task(title=request.POST['title'], 
-        due_at=make_aware(parse_datetime(request.POST['due_at'])))
+        task = Task(
+            title=request.POST['title'],
+            due_at=make_aware(parse_datetime(request.POST['due_at']))
+        )
 
         task.save()
 
